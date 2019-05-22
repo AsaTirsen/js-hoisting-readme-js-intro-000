0728da60abf8e1119353b3c6382669fc3044883a
Asa Tirsen <asa.tirsen@gmail.com>
2019-05-22 02:55:55 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -9,11 +9,11 @@ function callMe() {
 }
 
 
-function crazy() {
+var crazy = function () {
   // fix the code in here:
   var thisIsCrazy = function (){
     console.log("hey!!!")
-  } thisIsCrazy()();
+  } thisIsCrazy();
 }
 
 
